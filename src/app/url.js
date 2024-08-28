c5403442680feb2692c5dbef3937155ca41a3317
vaibhavj478 <vaibhavj478@gmail.com>
2024-08-29 00:53:37 +0530

    admin url update

--- src/app/url.js
+++ src/app/url.js
@@ -1,6 +1,7 @@
 
 
-const url  =  `http://localhost:8080/api-v1`
+// const url  =  `http://localhost:8080/api-v1`
 // const url  =  `https://mansa-backend.onrender.com/api-v1`
+const url  =  `https://jsm-backend-n95q.onrender.com/api-v1`
 
 export default url 
